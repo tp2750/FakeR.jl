de1e15336083ad8cfe10d63d00821e2905ea094a
Documenter.jl <documenter@juliadocs.github.io>
2022-09-27 18:36:27 +0000

    build based on b53aa0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FakeR","category":"page"},{"location":"#FakeR","page":"Home","title":"FakeR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FakeR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FakeR]","category":"page"}]
+}
